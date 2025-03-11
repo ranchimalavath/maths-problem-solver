e20eafa4f741e159bc547e40ff6766f54c9bf2fb
ranchimalavath <ranchimalavath@gmail.com>
2025-03-11 11:23:13 +0000

    Add 6.py

---
+++ src/6.py
@@ -0,0 +1,10 @@
+
+import random
+
+def solve_math_problem(problem):
+    if problem == "2+2":
+        return 4
+    elif problem == "10-5":
+        return 5
+    else:
+        return "Invalid problem"
