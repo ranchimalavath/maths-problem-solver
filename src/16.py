70d31823d239d37cd2a598b04c7da984df6ff245
ranchimalavath <ranchimalavath@gmail.com>
2025-03-22 16:46:18 +0000

    Add 16.py

---
+++ src/16.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    else:
+        return a / b
