8579a24a35214b3575e056037459928f6a780cd1
ranchimalavath <ranchimalavath@gmail.com>
2025-04-05 06:21:36 +0000

    Add 25.py

---
+++ src/25.py
@@ -0,0 +1,11 @@
+def calculate(x, y, operation):
+    if operation == "add":
+        return x + y
+    elif operation == "subtract":
+        return x - y
+    elif operation == "multiply":
+        return x * y
+    elif operation == "divide":
+        return x / y
+
+print(calculate(10, 5, "add"))
