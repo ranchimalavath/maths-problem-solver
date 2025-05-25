af839182821bb764e7e3982c1c618c6a7ef2bdd5
ranchimalavath <ranchimalavath@gmail.com>
2025-05-25 14:52:41 +0000

    Add 38.py

---
+++ src/38.py
@@ -0,0 +1,16 @@
+# Example 1: Simple arithmetic operations
+print(2 + 3)
+print(4 - 1)
+
+# Example 2: Basic conditional statements
+age = 25
+if age > 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# Example 3: Looping through a range of numbers
+numbers = list(range(1, 10))
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
